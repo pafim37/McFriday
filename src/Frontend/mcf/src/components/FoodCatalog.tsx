d54c9de1ddfe86a9ba6312ed0f117eca8e6d8e0a
pafim37 <astropawelpiatek@gmail.com>
2024-04-26 23:07:07 +0200

    refactor: food catalog component

--- src/Frontend/mcf/src/components/FoodCatalog.tsx
+++ src/Frontend/mcf/src/components/FoodCatalog.tsx
@@ -1,8 +1,12 @@
 import { Typography } from "@mui/material";
 import React from "react";
 
-const FoodCatalog : React.FC = (props) => {
-    return <Typography variant="h3" className="header">Witaj w restauracji McFriday</Typography>
-}
+const FoodCatalog: React.FC = () => {
+  return (
+    <Typography variant="h3" className="header">
+      Witamy w restauracji McFriday
+    </Typography>
+  );
+};
 
 export default FoodCatalog;
