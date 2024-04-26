2d80185a39b4d2325e2ce3b45f709dcbac81e377
pafim37 <astropawelpiatek@gmail.com>
2024-04-26 22:31:51 +0200

    feat: create food catalog component

--- src/Frontend/mcf/src/App.tsx
+++ src/Frontend/mcf/src/App.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import EatPlaceChoose from "./components/EatPlaceChoose.tsx";
-import { Grid } from "@mui/material";
+import FoodCatalog from "./components/FoodCatalog.tsx";
 
 const App = () => {
   const [place, setPlace] = React.useState<string>("");
 
-  return (
-    <Grid container id="containerP" alignItems="center" sx={{ minHeight: "100vh"}}>
-      {place == "" && <EatPlaceChoose setPlace={setPlace} />}
-    </Grid>
+  return place === "" ? (
+    <EatPlaceChoose setPlace={setPlace} />
+  ) : (
+    <FoodCatalog />
   );
 };
 
---
+++ src/Frontend/mcf/src/components/FoodCatalog.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const FoodCatalog : React.FC = (props) => {
+    return <Typography variant="h3" className="header">Witaj w restauracji McFriday</Typography>
+}
+
+export default FoodCatalog;
