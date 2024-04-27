7636c03ec09281a58cf7b50b2e951e7d2fa5587e
pafim37 <astropawelpiatek@gmail.com>
2024-04-27 17:15:16 +0200

    feat: display products

--- src/Frontend/mcf/src/App.tsx
+++ src/Frontend/mcf/src/App.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import axios from "axios";
 import EatPlaceChoose from "./components/EatPlaceChoose.tsx";
 import FoodCatalog from "./components/FoodCatalog.tsx";
 
 const App = () => {
   const [place, setPlace] = React.useState<string>("");
+  const [productTypes, setProductTypes] = React.useState<Array<ProductType>>([]); // TODO: change any
+  const baseUrl = "http://localhost:5226/"
+
+  React.useEffect(() => {
+    axios.get(baseUrl).then((response) => {
+      setProductTypes(response.data);
+      console.log(response.data);
+    }).catch(error => console.log(error));
+  }, []);
 
   return place === "" ? (
     <EatPlaceChoose setPlace={setPlace} />
   ) : (
-    <FoodCatalog />
+    <FoodCatalog productTypes={productTypes}/>
   );
 };
 
--- src/Frontend/mcf/src/components/FoodCatalog.tsx
+++ src/Frontend/mcf/src/components/FoodCatalog.tsx
@@ -1,11 +1,54 @@
-import { Typography } from "@mui/material";
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
-const FoodCatalog: React.FC = () => {
+interface FoodCatalogProps {
+  productTypes: Array<ProductType>;
+}
+
+const FoodCatalog: React.FC<FoodCatalogProps> = (props) => {
+  const [currIndex, setCurrIndex] = React.useState<number>(0);
+  const [currProducts, setCurrProducts] = React.useState<Array<Product>>([]);
+
+  const handleClick = (index: number, productType: ProductType) => {
+    setCurrIndex(index);
+    setCurrProducts(productType.products);
+  };
+
   return (
-    <Typography variant="h3" className="header">
-      Witamy w restauracji McFriday
-    </Typography>
+    <>
+      <Typography variant="h3" className="header">
+        Witamy w restauracji McFriday
+      </Typography>
+      <Grid container>
+        <Grid item xs={2}>
+          <List>
+            {props.productTypes.map((productType, index) => (
+              <ListItem key={index}>
+                <ListItemButton
+                  selected={currIndex === index}
+                  onClick={() => handleClick(index, productType)}
+                >
+                  {productType.name}
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={10}>
+          <Grid container>
+            {currProducts.map((p) => (
+              <Grid xs={4}>{p.name}</Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
